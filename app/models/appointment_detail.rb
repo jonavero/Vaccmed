c8a7775e254047b175fa33f89cf79551c3b77e0e
Desarrollo <alexander0-5@hotmail.com>
2021-10-03 16:07:39 -0400

    add update for appointments

--- app/models/appointment_detail.rb
+++ app/models/appointment_detail.rb
@@ -2,6 +2,9 @@ class AppointmentDetail < ApplicationRecord
   belongs_to :appointment, optional: false
   belongs_to :vaccine
 
+  alias_attribute :vaccineId, :vaccine_id
+
+
   VALID_STATUS=['Creada','Pendiente','Realizada']
   validates :status, inclusion: {in:VALID_STATUS}
 end
