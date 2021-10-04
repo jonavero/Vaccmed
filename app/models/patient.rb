c00099056ead6aaba0533860ffc598e5ad8d8387
Desarrollo <alexander0-5@hotmail.com>
2021-10-03 20:25:58 -0400

    validate email uniqueness

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,5 +3,4 @@ class Patient < ApplicationRecord
   VALIDATE_GENDER =['Femenina','Masculino']
   validates :gender,inclusion: {in:VALIDATE_GENDER}
   validates_presence_of :name,:surname,:gender,:birthday,:createdBy
-  validates_uniqueness_of :identityCard
 end
