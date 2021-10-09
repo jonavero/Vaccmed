b767aa54bef42d16194dd0dffb2ab4515a27f113
Desarrollo <alexander0-5@hotmail.com>
2021-10-08 20:38:48 -0400

    filter in appointments

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::AppointmentsController < ApplicationController
   before_action :set_appointment, only: [:show,:update,:destroy]
   def index
-    @appointments = if params[:status] !=""
+    @appointments = if params[:status] !=''
                       @count=  Appointment.joins(:patient,:tutor,:appointment_details ).where('appointments.status = ?' ,params[:status] ).uniq.count
                       Appointment.joins(:patient,:tutor,:appointment_details ).where('appointments.status = ?',params[:status] ).paginate(:page => params[:skip], :per_page => params[:maxCount]).order(:id).uniq
                     else
                       @count =Appointment.count
-                      Appointment.joins(:patient,:tutor,:appointment_details ).paginate(:page => params[:skip], :per_page => params[:maxCount]).order(:id).uniq
+                      Appointment.paginate(:page => params[:skip], :per_page => params[:maxCount]).order('appointments.id').uniq
                     end
   end
 
