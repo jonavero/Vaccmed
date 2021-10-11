27c09383a7bc26f4d8ffc84ea989ca72d012df95
Desarrollo <alexander0-5@hotmail.com>
2021-10-10 22:41:27 -0400

    add reportCenter

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -2,10 +2,16 @@ class  Api::V1::ReportsController < ApplicationController
 
   def reportCenter
    @mensaje ='Parametros no suministrados'
-    if params[:idCenter] && params[:fechaInicio] && params[:fechafinal] && params[:sexo] && params[:status]
-      @report = Appointment.where('branch_office_id=? and created_at>= ? and created_at <= ?',params[:idCenter],params[:fechaInicio],params[:fechafinal])
+    if params[:idCenter] !='' && params[:fechaInicio] && params[:fechafinal]  && params[:status] !=''
+      @count = Appointment.where('branch_office_id=?  and status=? and created_at::date>= ? and created_at::date <= ?',params[:idCenter],params[:status],params[:fechaInicio],params[:fechafinal]).count
+      @report = Appointment.where('branch_office_id=?  and status=? and created_at::date>= ? and created_at::date <= ?',params[:idCenter],params[:status],params[:fechaInicio],params[:fechafinal])
+
+    elsif  params[:fechaInicio] && params[:fechafinal]  && params[:status] !=''
+      @count = Appointment.where('status=? and created_at::date>= ? and created_at::date <= ?',params[:status],params[:fechaInicio],params[:fechafinal]).count
+      @report = Appointment.where('status=? and created_at::date>= ? and created_at::date <= ?',params[:status],params[:fechaInicio],params[:fechafinal])
     else
-      @report = Appointment.where('created_at>= ? and created_at <= ?',params[:fechaInicio],params[:fechafinal])
+      @count = Appointment.where('created_at::date>= ? and created_at::date <= ?',params[:fechaInicio],params[:fechafinal]).count
+      @report = Appointment.where('created_at::date>= ? and created_at::date <= ?',params[:fechaInicio],params[:fechafinal])
     end
 
 
--- app/views/api/v1/reports/reportCenter.jbuilder
+++ app/views/api/v1/reports/reportCenter.jbuilder
@@ -1,8 +1,10 @@
+json.totalCount @count
 json.reportCenter @report do |r|
   json.center r.branch_office.name
   json.patientName r.patient.name
   json.patientSurname r.patient.surname
-  json.patient r.appointmentDate
-  json.patient r.created_at.strftime("%d-%m-%Y")
+  json.appointmentDate r.appointmentDate
+  json.status r.status
+  json.createdAt r.created_at.strftime("%d-%m-%Y")
 
 end
